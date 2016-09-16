fae8a42cb619c1a7cc070a4de16cf72c3aa1a1c1
Yael Stein <yaelst@gmail.com>
2016-09-16 16:05:45 +0200

    finished search

--- search2.js
+++ search2.js
@@ -1,3 +1,4 @@
+
 (function(){
 
     var countries = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla", "Antigua", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bonaire (Netherlands Antilles)", "Bosnia Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Congo, The Democratic Republic of", "Cook Islands", "Costa Rica", "Croatia", "Curacao (Netherlands Antilles)", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iraq", "Ireland (Republic of)", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kosrae Island", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Lithuania", "Luxembourg", "Macau", "Macedonia (FYROM)", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Moldova", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Ponape", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Rota", "Russia", "Rwanda", "Saba (Netherlands Antilles)", "Saipan", "Samoa", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "South Africa", "South Korea", "Spain", "Sri Lanka", "St. Barthelemy", "St. Croix", "St. Eustatius (Netherlands Antilles)", "St. John", "St. Kitts and Nevis", "St. Lucia", "St. Maarten (Netherlands Antilles)", "St. Thomas", "St. Vincent and the Grenadines", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Tinian", "Togo", "Tonga", "Tortola", "Trinidad and Tobago", "Truk", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos", "Tuvalu", "US Virgin Islands", "Uganda", "Ukraine", "Union Island", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Gorda", "Wallis and Futuna", "Yap", "Yemen", "Zambia", "Zimbabwe"];
@@ -5,16 +6,12 @@
     var textArea = document.getElementById("text");
     var counter=0;
     var addCountry="";
-    var targetText;
-    var targetElement=null;
+    var bold;
     var index;
     var height = document.getElementById("results").offsetHeight;
     var results = document.getElementById("results");
     textArea.addEventListener("input", function(){
 
-        index=0;
-        targetElement=null;
-        //targetText=null;
 
         height = 0;
 
@@ -23,6 +20,7 @@
             results.innerHTML = "";
         }
 
+        if(results.innerHTML === "")  {index=-1;}
 
         var matchCountries = countries.filter(function(name){
             return (name.slice(0, textArea.value.length).toLowerCase()===textArea.value.toLowerCase());
@@ -34,11 +32,6 @@
             counter = 1;
         }
 
-
-        //    for (var j=0;j<counter;j++) {
-        //        var a = document.getElementById("body");
-        //      console.log(document.getElementsByClassName("country")[j]);
-        //    }
 
 
 
@@ -59,14 +52,6 @@
             height+=40;
             results.style.height = height + "px";
 
-
-            //    var newP = document.createElement("p");
-            //    var option = document.createTextNode(matchCountries[i]);
-
-
-            //    newP.appendChild(option);
-            //    document.body.appendChild(newP);
-            //    newP.classList.add("country");
         }
 
         document.addEventListener("click", function(e){
@@ -79,106 +64,53 @@
             e.stopPropagation();
         });
 
-        results.addEventListener('mouseover', function(e) {
-            if(e.target.tagName=="P"){
-                targetText = e.target.innerHTML;
-
-                e.target.style.color = "blue";
-                e.target.style.backgroundColor = "yellow";
-                targetElement = e.target;
-            }
-
-            results.addEventListener('mouseout', function(e) {
-                if(e.target.tagName=="P"){
-                    e.target.style.color = "black";
-                    e.target.style.backgroundColor = "white";
-                }});
 
 
 
-
-            });
-
-
-
-
-        })
 
 
         document.addEventListener('keydown', function(e) {
 
             if (e.keyCode===40) {
 
+                index+=1;
+
+console.log(index);
+
+                if(index<0) {index=0;}
+                if(index>3) {index=3}
+
+                document.getElementsByTagName("P")[index].style.color = "blue";
+                document.getElementsByTagName("P")[index].style.backgroundColor = "yellow";
 
 
-                if (targetElement===null){
-
-
-
-                    index=0;
-                    targetElement = document.getElementsByTagName("P")[0];
-                    targetText = document.getElementsByTagName("P")[0].innerHTML;
-                    document.getElementsByTagName("P")[index].style.color = "blue";
-                    document.getElementsByTagName("P")[index].style.backgroundColor = "yellow";
-                    //    index = matchCountries.indexOf(targetText);
-                    console.log(targetText);
-
-
+                if (index>0) {
+                    document.getElementsByTagName("P")[index-1].style.color = "black";
+                    document.getElementsByTagName("P")[index-1].style.backgroundColor = "white";
 
                 }
-                else {
 
-                    document.getElementsByTagName("P")[index].style.color = "black";
-                    document.getElementsByTagName("P")[index].style.backgroundColor = "white";
-
-                    document.getElementsByTagName("P")[index+1].style.color = "blue";
-                    document.getElementsByTagName("P")[index+1].style.backgroundColor = "yellow";
-                    index+=1;
-                    if(index>3) {index=0;}
-                    targetText = document.getElementsByTagName("P")[index].innerHTML;
-                    console.log(targetText);
-
-
-                };
             };
 
 
             if (e.keyCode===38) {
-
-                document.getElementsByTagName("P")[index].style.color = "black";
-                document.getElementsByTagName("P")[index].style.backgroundColor = "white";
-                document.getElementsByTagName("P")[index-1].style.color ="blue";
-                document.getElementsByTagName("P")[index-1].style.backgroundColor = "yellow";
-                index-=1;
+                index -= 1;
                 if(index<0) {index=0;}
-                targetText = document.getElementsByTagName("P")[index].innerHTML;
+                if(index>3) {index=3}
 
-            }
-            if (e.keyCode===13) {
-                textArea.value = targetText;
-
-                for (var j=0;j<counter+1;j++){
-                    results.innerHTML = "";
-                }
-                results.style.height="0px";
+                document.getElementsByTagName("P")[index+1].style.color = "black";
+                document.getElementsByTagName("P")[index+1].style.backgroundColor = "white";
+                document.getElementsByTagName("P")[index].style.color ="blue";
+                document.getElementsByTagName("P")[index].style.backgroundColor = "yellow";
+                //        index-=1;
+                //        targetText = document.getElementsByTagName("P")[index].innerHTML;
 
             }
 
 
         });
 
-
-    results.addEventListener('click', function(e) {
-        if(e.target.tagName=="P"){
-            textArea.value = e.target.innerHTML;
-
-            for (var j=0;j<counter+1;j++){
-                results.innerHTML = "";
-            }
-            results.style.height="0px";            }
-
-    });
+    })
 
 
-
-    })();
+})();
